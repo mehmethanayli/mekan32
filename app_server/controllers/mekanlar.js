274e9b0865b2d6b5b5657d1a95c5225a813e581f
Mehmet Can Hanaylı <mehmet.can.hanayli@gmail.com>
2020-11-29 14:32:48 +0300

    Odev2: Örnek 5 tane Mekan eklenmiştir. Controller ve Route configurasyonları yapilarak pug dosyalari olusturulmustur.

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -6,6 +6,7 @@ const anaSayfa = function(req, res, next) {
     res.render('mekanlar-liste', { title: 'Anasayfa' });
 }
 
+
 const mekanBilgisi = function(req, res, next) {
     res.render('mekan-detay', { title: 'Mekan Bilgisi' });
 }
@@ -14,10 +15,37 @@ const yorumEkle = function(req, res, next) {
     res.render('yorum-ekle', { title: 'Yorum Ekle' });
 }
 
+//Mekanlar için Eklenen
+const kahveDeryasi = function(req, res, next) {
+    res.render('mekan-kahveDeryasi', { title: 'Kahve Deryası' });
+}
+
+const kahveDeresi = function(req, res, next) {
+    res.render('mekan-kahveDeresi', { title: ' Kahve Deresi' });
+}
+
+const kahveDiyari = function(req, res, next) {
+    res.render('mekan-kahveDiyari', { title: 'Kahve Diyarı' });
+}
+
+const kahveDunyasi = function(req, res, next) {
+    res.render('mekan-kahveDunyasi', { title: 'Kahve Dünyası' });
+}
+
+const kazakKahvesi = function(req, res, next) {
+    res.render('mekan-kazakKahvesi', { title: 'Kazak Kahvesi' })
+}
+
+
 module.exports = {
     anaSayfa,
     mekanBilgisi,
-    yorumEkle
+    yorumEkle,
+    kahveDeryasi,
+    kahveDeresi,
+    kahveDiyari,
+    kahveDunyasi,
+    kazakKahvesi
 }
 
 module.exports.admin = function(req, res, next) {
